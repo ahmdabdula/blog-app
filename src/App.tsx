8300828609692f2c223ef746cd0f5406d5a5263f
Ahmad Abdullah <91375330+ahmdabdula@users.noreply.github.com>
2023-04-12 19:16:47 +0500

    feat: create-blog feature
    * feat: login signup UI (#1)
    
    * feat: add login/signup ui
    
    * feat: add validation and alert
    
    * feat: make login signup responsive
    
    * fix: fix field validation
    
    * fix: fix field validation and alert
    
    * fix: routes,naming-conventions,imports
    
    * fix: file names
    
    * fix: file names
    
    * Delete src/Routes/login directory
    
    * Delete src/Routes/signup directory
    
    * Delete src/components directory
    
    * Delete src/utils directory
    
    * fix: self closing tags
    
    * feat: add firebase authentication

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,16 @@
-import React from "react";
-import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import Home from "./Routes/HomePage/homePage";
+import Login from "./Routes/Login/login";
+import Signup from "./Routes/Signup/signup";
 
-function App() {
-  return <div className="App"></div>;
-}
+const App = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="login" element={<Login />} />
+      <Route path="signup" element={<Signup />} />
+    </Routes>
+  );
+};
 
 export default App;
---
+++ src/Utils/Firebase/firebase.utils.ts
@@ -0,0 +1,72 @@
+import { initializeApp } from "firebase/app";
+import {
+  getAuth,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+} from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAyA6oFDi1rqvhFYhNy1PbLLD4QW8Wf3Xs",
+  authDomain: "blog-app-90794.firebaseapp.com",
+  projectId: "blog-app-90794",
+  storageBucket: "blog-app-90794.appspot.com",
+  messagingSenderId: "979226588983",
+  appId: "1:979226588983:web:6d1e0778e93992e2e74cb2",
+};
+
+const firebaseApp = initializeApp(firebaseConfig);
+
+export const auth = getAuth();
+
+export const db = getFirestore();
+
+export type AdditionalInformation = {
+  displayName?: string;
+};
+
+export type UserData = {
+  displayName: string;
+  email: string;
+};
+
+const generateErrorMessage = (code: string) => {
+  switch (code) {
+    case "auth/email-already-in-use":
+      return "user with this email already exists";
+    case "auth/wrong-password":
+      return "password is incorrect";
+    case "auth/user-not-found":
+      return "user with this email does not exist";
+    default:
+      return "error occurred";
+  }
+};
+
+export const createAuthUserWithEmailAndPassword = async (
+  email: string,
+  password: string
+) => {
+  if (!email || !password) return;
+
+  try {
+    return await createUserWithEmailAndPassword(auth, email, password);
+  } catch (error) {
+    const code = (error as { code: string }).code;
+    throw generateErrorMessage(code);
+  }
+};
+
+export const signInAuthUserWithEmailAndPassword = async (
+  email: string,
+  password: string
+) => {
+  if (!email || !password) return;
+  try {
+    return await signInWithEmailAndPassword(auth, email, password);
+  } catch (error) {
+    const code = (error as { code: string }).code;
+    console.log(code);
+    throw generateErrorMessage(code);
+  }
+};
